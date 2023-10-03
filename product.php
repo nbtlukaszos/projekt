374d7ffdb4e30abfe5cc2aa9f04b42776eb4bfa2
nbtlukaszos <144244245+nbtlukaszos@users.noreply.github.com>
2023-10-03 21:28:14 +0200

    Update product.php
    Signed-off-by: nbtlukaszos <144244245+nbtlukaszos@users.noreply.github.com>

--- product.php
+++ product.php
@@ -11,7 +11,7 @@ $email    = "";
 $errors = array(); 
 
 
-$db = mysqli_connect('localhost', 'root', '', 'project');
+$db = mysqli_connect('sql201.infinityfree.com', 'if0_35160150', 'tAadYVDe5r', 'if0_35160150_project');
 
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
