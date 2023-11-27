351bcd7b895d56d9ede9531cf1b700080d061619
Hubertnd <144244857+Hubertnd@users.noreply.github.com>
2023-11-27 21:28:32 +0100

    Update product.php
    Signed-off-by: Hubertnd <144244857+Hubertnd@users.noreply.github.com>

--- product.php
+++ product.php
@@ -1,45 +1,65 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<?php
-session_start();
-
-
-$username = "";
-$email    = "";
-$errors = array(); 
-
-
-$db = mysqli_connect('sql201.infinityfree.com', 'if0_35160150', 'tAadYVDe5r', 'if0_35160150_project');
-
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
+<?php include('server.php') ;
+if(isset($_GET['delete_id'])) {
+    $delete_id = $_GET['delete_id'];
+    $sql_delete = "DELETE FROM produkty WHERE ID = $delete_id";
+    if ($db->query($sql_delete) === TRUE) {
+        echo "Wiersz został usunięty.";
+    } else {
+        echo "Błąd usuwania wiersza: " . $db->error;
+    }
 }
 
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nazwa = $_POST['nazwa'];
+    $kategoria = $_POST['kategoria'];
+    $cena = $_POST['cena'];
 
-$sql = "SELECT id, username, email, img FROM users";
+    $sql_insert = "INSERT INTO produkty (Nazwa, Kategoria, Cena) VALUES ('$nazwa', '$kategoria', '$cena')";
+    if ($db->query($sql_insert) === TRUE) {
+        echo "Nowy wiersz został dodany.";
+    } else {
+        echo "Błąd dodawania wiersza: " . $db->error;
+    }
+}
+
+
+$sql = "SELECT * FROM produkty";
 $result = $db->query($sql);
 
 
 if ($result->num_rows > 0) {
-   
+ 
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Nazwa</th><th>Kategoria</th><th>Cena</th><th>Akcja</th></tr>";
     while($row = $result->fetch_assoc()) {
-        print "<br> id: ". $row["id"]. "<br> - Name: ". $row["username"]. "<br> - Email: " . $row["email"] . "<br>";
-      print "<img src=\"".$row["img"]."\">";
-     
+        echo "<tr><td>" . $row["ID"] . "</td><td>" . $row["Nazwa"] . "</td><td>" . $row["Kategoria"] . "</td><td>" . $row["Cena"] . "</td><td><a href='?delete_id=" . $row["ID"] . "'>Usuń</a></td></tr>";
     }
+    echo "</table>";
 } else {
-    print "0 results";
+    echo "Brak danych do wyświetlenia.";
 }
-
-
-
-$db->close();   
-        ?> 
-
-
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styladmin.css">
+</head>
+<body>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <label for="nazwa">Nazwa:</label>
+    <input type="text" id="nazwa" name="nazwa"><br><br>
+    <label for="kategoria">Kategoria:</label>
+    <input type="text" id="kategoria" name="kategoria"><br><br>
+    <label for="cena">Cena:</label>
+    <input type="text" id="cena" name="cena"><br><br>
+    <input type="submit" value="Dodaj">
+</form>
+<form method="get" action="admin.php">
+    <input type="submit" value="Powrót do Panelu głownego">
+    </form>
 </body>
 </html>
